9be4995205ee20a5a742c5dfe48816fc011045bc
Cameron Eckelberry <cameck@gmail.com>
2016-06-08 19:12:41 -0400

    Added dashboard and startscreen classes

--- src/components/StartScreen/index.js
+++ src/components/StartScreen/index.js
@@ -1,10 +1,9 @@
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <p>
-        Hello, <input type="text" placeholder="Your name here" />!
-        It is {this.props.date.toTimeString()}
-      </p>
+import React, {Component} from 'react';
+
+export default class StartScreen extends Component {
+  render() {
+    return(
+      <div>WEEE</div>
     );
   }
-});
+}
